263bceb2e9f8d9d064bfe561e052f176712ccffb
unknown <239138@TECH.WYB.AC.LK>
2025-03-26 20:28:07 +0530

    first commit

---
+++ if.py
@@ -0,0 +1,10 @@
+x=211
+
+if (x>10):
+    print("x is grater than 10")
+elif x>20:
+    print("x is grater than 20")
+else:
+    print("x is not grater than 10")
+    
+    
---
+++ max.py
@@ -0,0 +1,10 @@
+x=0
+max=0
+while x<5:
+    num = int(input("Enter the number: "))
+    x+=1
+    if max<num:
+        max = num
+    
+
+print(max)
