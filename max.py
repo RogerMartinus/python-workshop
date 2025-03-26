ed380140ef9e123e207939be064b7b9170da014a
unknown <239138@TECH.WYB.AC.LK>
2025-03-26 20:36:00 +0530

    first commit

--- max.py
+++ max.py
@@ -2,6 +2,7 @@ x=0
 max=0
 while x<5:
     num = int(input("Enter the number: "))
+    array[]
     x+=1
     if max<num:
         max = num
---
+++ task 4.py
@@ -0,0 +1,6 @@
+x=0
+
+while x<100:
+    if x%7==0:
+        print(x)
+    x+=1
